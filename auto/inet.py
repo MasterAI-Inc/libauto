1b2c0b2bc9f7c05a7d08c8fd7025d2ff61b763fe
Ryan Henning <ryan@rhobota.com>
2019-05-22 20:07:48 +0000

    Can check for an internet connection with max 4 second timeout

--- auto/inet.py
+++ auto/inet.py
@@ -9,10 +9,11 @@
 ###############################################################################
 
 import subprocess
-
 import socket
 import fcntl
 import struct
+import signal
+from contextlib import contextmanager
 
 
 class Wireless:
@@ -25,14 +26,6 @@ class Wireless:
     def __init__(self, interface=None):
         self.interface = interface
 
-    def _delete_connection(self, ssid_to_delete):
-        response = _run_cmd("nmcli --terse --fields UUID,NAME,TYPE con show".split(' '))
-
-        for line in response.splitlines():
-            uuid, name, type_ = line.split(':')
-            if type_ == '802-11-wireless' and ssid_to_delete in name:
-                _run_cmd("nmcli con delete uuid".split(' ') + [uuid])
-
     def _error_in_response(self, response):
         for line in response.splitlines():
             if line.startswith('Error'):
@@ -40,7 +33,7 @@ class Wireless:
         return False
 
     def connect(self, ssid, password):
-        self._delete_connection(ssid)
+        self.delete_connection(ssid)
 
         response = _run_cmd("nmcli dev wifi connect".split(' ') +
                             [ssid, 'password', password, 'ifname', self.interface,
@@ -49,13 +42,9 @@ class Wireless:
         did_connect = not self._error_in_response(response)
 
         if not did_connect:
-            self._delete_connection(ssid)
+            self.delete_connection(ssid)
 
         return did_connect
-
-    def forget_current(self):
-        name = self.current()
-        self._delete_connection(name)
 
     def current(self):
         response = _run_cmd("nmcli --terse --field DEVICE,CONNECTION dev".split(' '))
@@ -66,6 +55,14 @@ class Wireless:
                 return name
 
         return None
+
+    def delete_connection(self, ssid_to_delete):
+        response = _run_cmd("nmcli --terse --fields UUID,NAME,TYPE con show".split(' '))
+
+        for line in response.splitlines():
+            uuid, name, type_ = line.split(':')
+            if type_ == '802-11-wireless' and ssid_to_delete in name:
+                _run_cmd("nmcli con delete uuid".split(' ') + [uuid])
 
     def radio_power(self, on=None):
         if power is True:
@@ -87,6 +84,58 @@ def get_ip_address(ifname):
         )[20:24])
     except:
         return None
+
+
+def _raise_error(signum, frame):
+    """This handler will raise an error inside gethostbyname"""
+    raise OSError
+
+
+@contextmanager
+def _set_signal(signum, handler):
+    """Temporarily set signal"""
+    old_handler = signal.getsignal(signum)
+    signal.signal(signum, handler)
+    try:
+        yield
+    finally:
+        signal.signal(signum, old_handler)
+
+
+@contextmanager
+def _set_alarm(time):
+    """Temporarily set alarm"""
+    signal.setitimer(signal.ITIMER_REAL, time)
+    try:
+        yield
+    finally:
+        signal.setitimer(signal.ITIMER_REAL, 0) # Disable alarm
+
+
+@contextmanager
+def _raise_on_timeout(time):
+    """This context manager will raise an OSError unless
+    The with scope is exited in time."""
+    with _set_signal(signal.SIGALRM, _raise_error):
+        with _set_alarm(time):
+            yield
+
+
+def has_internet_access():
+    try:
+        with _raise_on_timeout(2.0):
+            params = socket.getaddrinfo('ws.autoauto.ai', 'https', proto=socket.IPPROTO_TCP)[0]
+    except OSError:
+        return False
+    family, type_, proto = params[:3]
+    sockaddr = params[4]
+    sock = socket.socket(family, type_, proto)
+    sock.settimeout(2.0)
+    try:
+        sock.connect(sockaddr)   # <-- blocking, but respects the `settimeout()` call above
+        return True
+    except socket.timeout:
+        return False
 
 
 def list_ifaces():
