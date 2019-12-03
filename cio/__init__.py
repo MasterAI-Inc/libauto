bbf6e7b9cef52fc17531bbfe1fdaec5e108479a0
Ryan Henning <ryan@rhobota.com>
2019-12-03 21:18:47 +0000

    Formalizing interface for ALL controllers

--- cio/__init__.py
+++ cio/__init__.py
@@ -1,6 +1,5 @@
 """
-This package contains the interface to the microcontroller which we communicate
-with via I2C from this host.
+This package contains the interface to your robot's microcontroller(s).
 
 It's name is CIO (Controller Input/Output).
 """
@@ -8,19 +7,31 @@ It's name is CIO (Controller Input/Output).
 import abc
 
 
-#'Accelerometer': {'is_enabled': False, 'register_number': 13},
-#'Calibrator': {'is_enabled': False, 'register_number': 16},
-#'CarMotors': {'is_enabled': False, 'register_number': 11},
-#'Encoders': {'is_enabled': False, 'register_number': 7},
-#'Gyroscope': {'is_enabled': False, 'register_number': 14},
-#'Gyroscope_accum': {'is_enabled': False, 'register_number': 15},
-#'LEDs': {'is_enabled': False, 'register_number': 5},
-#'LoopFrequency': {'is_enabled': False, 'register_number': 10},
-#'PID_steering': {'is_enabled': False, 'register_number': 12},
-#'Photoresistor': {'is_enabled': False, 'register_number': 6},
-#'PushButtons': {'is_enabled': False, 'register_number': 4},
-#'Timer1PWM': {'is_enabled': False, 'register_number': 8},
-#'Timer3PWM': {'is_enabled': False, 'register_number': 9},
+"""
+Todo:
+    - 'Calibrator'
+    - 'PID_steering'
+    - 'Timer1PWM'
+    - 'Timer3PWM'
+
+Checklist for New Components:
+    - Double space between classes
+    - Single space between methods
+    - All classes end in 'Iface'
+    - All classes inherit 'abc.ABC'
+    - All classes have properly formatted docstring
+    - All classes have short and accurate docstring description
+    - All classes have `Required` set correctly
+    - All classes have the correct Capability Identifier
+    - All classes have same name as Capability Identifier
+    - All methods are decorated with `abc.abstractmethod`
+    - All methods take `self` as first parameter
+    - All methods have a docstring
+    - All methods have `pass` as the impl
+    - Correct, generic, simple methods to form interface of each class?
+    - All methods have a short and accuracte docstring
+    - All methods are async!
+"""
 
 
 class VersionInfoIface(abc.ABC):
@@ -33,10 +44,37 @@ class VersionInfoIface(abc.ABC):
     """
 
     @abc.abstractmethod
-    def version(self):
+    async def name(self):
+        """
+        Return the name of the controller (e.g. the product
+        name and model number of the robot) as a string.
+        There is no specific format for this string, it is
+        intended to by human readable.
+        """
+        pass
+
+    @abc.abstractmethod
+    async def version(self):
         """
         Return the major and minor version of the software running on the controller
-        as a tuple.
+        as a two-tuple.
+        """
+        pass
+
+
+class LoopFrequencyIface(abc.ABC):
+    """
+    Read the Controller's Loop Frequency (in Hz)
+
+    Required: True
+
+    Capability Identifier: 'LoopFrequency'
+    """
+
+    @abc.abstractmethod
+    async def read(self):
+        """
+        Returns the loop frequency of the microcontroller (in Hz).
         """
         pass
 
@@ -45,21 +83,29 @@ class BatteryVoltageReaderIface(abc.ABC):
     """
     Read the Battery Voltage
 
-    Required: False
+    Required: True
 
     Capability Identifier: 'BatteryVoltageReader'
     """
 
     @abc.abstractmethod
-    def millivolts(self):
+    async def millivolts(self):
         """
         Return the voltage of the battery (in millivolts) connected to the controller.
         Reminder: 1000 millivolts = 1 volt
         """
         pass
 
+    @abc.abstractmethod
+    async def minutes(self):
+        """
+        Estimate of time remaining (in minutes).
+        Return a two-tuple of the 95% confidence interval.
+        """
+        pass
 
-class Buzzer(abc.ABC):
+
+class BuzzerIface(abc.ABC):
     """
     Play Music!
 
@@ -69,7 +115,7 @@ class Buzzer(abc.ABC):
     """
 
     @abc.abstractmethod
-    def is_currently_playing(self):
+    async def is_currently_playing(self):
         """
         Return true if the buzzer is currently playing music.
         Return false if the buzzer is not. You cannot play new
@@ -79,24 +125,23 @@ class Buzzer(abc.ABC):
         pass
 
     @abc.abstractmethod
-    def wait(self):
+    async def wait(self):
         """
         Block until the buzzer is not playing music anymore.
         """
         pass
 
     @abc.abstractmethod
-    def play(self, notes="o4l16ceg>c8"):
+    async def play(self, notes="o4l16ceg>c8"):
         """
         Tell the controller to play the notes described by the `notes` parameter.
         If the controller is already playing notes, this function will wait for it
-        to finish then will play _these_ `notes`. (If the current notes do not finish
-        within 10 seconds, this function will give up and raise and error.)
+        to finish then will play _these_ `notes`.
         """
         pass
 
 
-class Gyroscope:
+class GyroscopeIface(abc.ABC):
     """
     Query the Gyroscope Sensor
 
@@ -105,10 +150,267 @@ class Gyroscope:
     Capability Identifier: 'Gyroscope'
     """
 
-    @i2c_retry(N_I2C_TRIES)
-    def read(self):
+    @abc.abstractmethod
+    async def read(self):
         """
-        Read an (x, y, z) tuple-of-floats from one of the controller's components.
-        E.g. Read from the Accelerometer, Gyroscope, or Magnetometer.
+        Read an (x, y, z) tuple-of-floats from the Gyroscope.
         """
+        pass
+
+
+class GyroscopeAccumIface(abc.ABC):
+    """
+    Query the Gyroscope Sensor with Internally-Accumulated Values
+
+    Required: False
+
+    Capability Identifier: 'Gyroscope_accum'
+    """
+
+    @abc.abstractmethod
+    async def reset(self):
+        """
+        Reset the (x, y, z) accumulators back to zero.
+        """
+        pass
+
+    @abc.abstractmethod
+    async def read(self):
+        """
+        Read the accumulated (x, y, z) tuple-of-floats from the Gyroscope.
+        """
+        pass
+
+
+class AccelerometerIface(abc.ABC):
+    """
+    Query the Accelerometer Sensor
+
+    Required: False
+
+    Capability Identifier: 'Accelerometer'
+    """
+
+    @abc.abstractmethod
+    async def read(self):
+        """
+        Read an (x, y, z) tuple-of-floats from the Accelerometer.
+        """
+        pass
+
+
+class PushButtonsIface(abc.ABC):
+    """
+    Query the Tactile Push Button(s)
+
+    Required: False
+
+    Capability Identifier: 'PushButtons'
+    """
+
+    @abc.abstractmethod
+    async def num_buttons(self):
+        """
+        Return the number of buttons on the device.
+        """
+        pass
+
+    @abc.abstractmethod
+    async def button_state(self, button_index):
+        """
+        Return the state of the button at the given index (zero-based).
+        Returns a tuple of (num_presses, num_releases, is_currently_pressed)
+        which are of types (int, int, bool).
+        """
+        pass
+
+    @abc.abstractmethod
+    async def get_events(self):
+        """
+        Return a list of buttons events that have happened since
+        the last call this method.
+        """
+        pass
+
+
+class LEDsIface(abc.ABC):
+    """
+    Set LED State
+
+    Required: False
+
+    Capability Identifier: 'LEDs'
+    """
+
+    @abc.abstractmethod
+    async def led_map(self):
+        """
+        Return identifiers and descriptions of the LEDs
+        available on this controller as a dictionary.
+        The keys are the identifiers used to control the LEDs
+        via the `set_led()` method.
+        """
+        pass
+
+    @abc.abstractmethod
+    async def set_led(self, led_identifier, val):
+        """
+        Set the LED on/off value.
+        """
+        pass
+
+    @abc.abstractmethod
+    async def mode_map(self):
+        """
+        Return identifiers and descriptions of the LED modes
+        that are available as a dictionary.
+        The keys are the identifiers used to set the mode
+        via the `set_mode()` method.
+        """
+        pass
+
+    @abc.abstractmethod
+    async def set_mode(self, mode_identifier):
+        """
+        Set the `mode` of the LEDs.
+        Pass `None` to clear the mode, thereby commencing
+        basic on/off control via the `set_led()` method.
+        """
+        pass
+
+
+class PhotoresistorIface(abc.ABC):
+    """
+    Query the Photoresistor
+
+    Required: False
+
+    Capability Identifier: 'Photoresistor'
+    """
+
+    @abc.abstractmethod
+    async def read(self):
+        """
+        Read the voltage of the photoresistor pin, and read
+        the computed resistance of the photoresistor. Return
+        both as a two-tuple `(millivolts, ohms)`.
+        """
+        pass
+
+    @abc.abstractmethod
+    async def read_millivolts(self):
+        """
+        Read the raw voltage of the photoresistor pin.
+        """
+        pass
+
+    @abc.abstractmethod
+    async def read_ohms(self):
+        """
+        Read the resistance of the photoresistor (in ohms). The photoresistor's
+        resistance changes depending on how much light is on it, thus the name!
+        """
+        pass
+
+
+class EncodersIface(abc.ABC):
+    """
+    Query the Quadrature Encoder(s)
+
+    Required: False
+
+    Capability Identifier: 'Encoders'
+    """
+
+    @abc.abstractmethod
+    async def num_encoders(self):
+        """
+        Return the number of Quadrature Encoders on this controller.
+        """
+        pass
+
+    @abc.abstractmethod
+    async def enable(self, encoder_index):
+        """
+        Enable an encoder. The first encoder is index-0.
+        """
+        pass
+
+    @abc.abstractmethod
+    async def read_counts(self, encoder_index):
+        """
+        Read the counts of the encoder. The first encoder is index-0.
+        The count is reset to zero when the encoder is enabled.
+
+        A three-tuple is returned:
+         - Number of "clicks" (a signed value).
+         - Pin A change count (an unsigned, positive value).
+         - Pin B change count (an unsigned, positive value).
+        """
+        pass
+
+    @abc.abstractmethod
+    async def read_timing(self, encoder_index):
+        """
+        Return the number of microseconds that each of the two pins on the first
+        encoder (e1) are high (i.e. the duration the pin stays in the high state).
+        This can be used to read a PWM signal (assuming the PWM signal has a known,
+        fixed frequency). If the pin has not gone high-then-low recently (within
+        the last half-second), the value returned here will be 0 to indicate
+        it is not presently known (i.e. the PWM signal is not currently coming
+        through). A two-tuple is returned by this method: `(pin_a_usecs, pin_b_usecs)`.
+        Recall: 1000000 microseconds = 1 second
+        """
+        pass
+
+    @abc.abstractmethod
+    async def disable(self, encoder_index):
+        """
+        Disable an encoder. The first encoder is index-0.
+        """
+        pass
+
+
+class CarMotorsIface(abc.ABC):
+    """
+    Control the Robots which are in the "Car Form Factor"
+
+    Required: False
+
+    Capability Identifier: 'CarMotors'
+    """
+
+    @abc.abstractmethod
+    async def on(self):
+        """
+        Tell the car to begin controlling the motors (the main motor and the servo).
+        The car will use the most recently set PWM signal parameters, or it will
+        read from its EEPROM to get those values.
+        """
+        pass
+
+    @abc.abstractmethod
+    async def set_steering(self, steering):
+        """
+        Set the car's steering (i.e. move the servo).
+
+        Pass a value in the range [-45, 45] to represent [full-right, full-left].
+        """
+        pass
+
+    @abc.abstractmethod
+    async def set_throttle(self, throttle):
+        """
+        Set the car's throttle (i.e. power send to the main motor).
+
+        Pass a value in the range [-100, 100] to represent [full-reverse, full-forward].
+        """
+        pass
+
+    @abc.abstractmethod
+    async def off(self):
+        """
+        Turn off the car's PWM motor control.
+        """
+        pass
 
