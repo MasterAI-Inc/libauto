0238025b36555a64ae9675d6a017885858326f58
Ryan Henning <ryan@masterai.ai>
2024-03-23 15:22:23 -0500

    Magnetometer!

---
+++ car/mag.py
@@ -0,0 +1,36 @@
+###############################################################################
+#
+# Copyright (c) 2017-2024 Master AI, Inc.
+# ALL RIGHTS RESERVED
+#
+# Use of this library, in source or binary form, is prohibited without written
+# approval from Master AI, Inc.
+#
+###############################################################################
+
+"""
+This module provides simplified implementations of the magnetometer sensor.
+"""
+
+from auto.asyncio_tools import thread_safe
+from auto.capabilities import list_caps, acquire
+
+
+def read():
+    """
+    Read an (x, y, z) tuple-of-floats from the Magnetometer.
+    """
+    return _get_mag().read()
+
+
+@thread_safe
+def _get_mag():
+    global _MAG
+    try:
+        _MAG
+    except NameError:
+        caps = list_caps()
+        if 'Magnetometer' not in caps:
+            raise AttributeError('This device has no Magnetometer sensor!')
+        _MAG = acquire('Magnetometer')
+    return _MAG
