4be27b68d66918679556397ccd79ddf70fedbb75
Ryan Henning <ryan@masterai.ai>
2024-03-24 22:05:15 -0500

    Remove redundancies

--- car/recon_weapons.py
+++ car/recon_weapons.py
@@ -19,6 +19,9 @@ from auto.asyncio_tools import thread_safe
 from auto.capabilities import list_caps, acquire
 from auto import IS_VIRTUAL
 
+from car import recon
+from car import weapons
+
 
 def naive_recon(theta_1, theta_2, slice_size, max_distance):
     """
@@ -29,7 +32,6 @@ def naive_recon(theta_1, theta_2, slice_size, max_distance):
     """
     if not IS_VIRTUAL:
         raise NotImplemented('This function only work on virtual cars.')
-    recon = _get_recon()
     theta_1, theta_2 = min(theta_1, theta_2), max(theta_1, theta_2)
     for a in range(theta_1, theta_2 - slice_size + 1, slice_size):
         vins = recon.query(a, a + slice_size, max_distance)
@@ -43,31 +45,4 @@ def throw_ball(theta, velocity):
     """
     if not IS_VIRTUAL:
         raise NotImplemented('This function only work on virtual cars.')
-    weapons = _get_weapons()
     weapons.fire(theta=theta, phi=90, velocity=velocity)
-
-
-@thread_safe
-def _get_recon():
-    global _RECON
-    try:
-        _RECON
-    except NameError:
-        caps = list_caps()
-        if 'Recon' not in caps:
-            raise AttributeError('This device has no Recon sensor!')
-        _RECON = acquire('Recon')
-    return _RECON
-
-
-@thread_safe
-def _get_weapons():
-    global _WEAPONS
-    try:
-        _WEAPONS
-    except NameError:
-        caps = list_caps()
-        if 'Weapons' not in caps:
-            raise AttributeError('This device has no Weapons system!')
-        _WEAPONS = acquire('Weapons')
-    return _WEAPONS
